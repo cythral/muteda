e945d368f3ebaaea0139ffca5516104af71410af
Talen Fisher <talen.fisher@cythral.com>
2020-04-28 20:23:16 -0500

    Revert "add test cluster starter service"
    This reverts commit a3819f7ceb58dc8985df72016f12b54bbfeefabd.

--- deploy/create-config-files.sh
+++ deploy/create-config-files.sh
@@ -5,7 +5,6 @@ cwd=$(dirname "${BASH_SOURCE[0]}")
 for file in $(ls ${cwd}/params/*.json); do
     envName=$(echo $file | xargs basename | sed "s/\.json//")
     params=$(cat $file)
-    params=$(echo $params | jq ".Image=\"$1\"")
     
     config={}
     config=$(echo $config | jq --argjson params "$params" '.Parameters=$params')
