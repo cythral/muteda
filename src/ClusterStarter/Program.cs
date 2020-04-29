a3819f7ceb58dc8985df72016f12b54bbfeefabd
Talen Fisher <talen.fisher@cythral.com>
2020-04-28 19:09:55 -0500

    add test cluster starter service

---
+++ src/ClusterStarter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace ClusterStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var port = 3306;
+            var localAddr = IPAddress.Parse("0.0.0.0");
+            var server = new TcpListener(localAddr, port);
+
+            server.Start();
+
+            while (true)
+            {
+                var client = server.AcceptTcpClient();
+                Task.Run(() => OnConnect(client));
+            }
+        }
+
+        static void OnConnect(TcpClient client)
+        {
+            var args = new SocketAsyncEventArgs();
+            args.SetBuffer(Encoding.UTF8.GetBytes("test"));
+            client.Client.SendAsync(args);
+        }
+    }
+}
